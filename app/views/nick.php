38e95ae530db6722a95d63fa38f4e713da8c5205
Nick Velloff <nick.velloff@gmail.com>
2014-11-08 21:11:31 -0500

    adding scale

--- app/views/nick.php
+++ app/views/nick.php
@@ -1,43 +1,75 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Nick's Test Route</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
+<meta charset="UTF-8">
+<title>BMI</title>
+<style>
 
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
+body {
+	margin: 0;
+}
 
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-		
-		a, a:visited {
-			text-decoration:none;
-		}
+.scale-container {
+	width: 590px;
+	height: 590px;
+	position: relative;
+    -webkit-mask: url(/img/assessment/scale/scale_mask.png) no-repeat;
+    -webkit-mask-position: 0px 0px;
+    
+    
+    -webkit-mask-image: url(img/assessment/scale/scale_mask.png);
+	-o-mask-image: url(img/assessment/scale/scale_mask.png);
+	-moz-mask-image: url(img/assessment/scale/scale_mask.png);
+	mask-image: url(img/assessment/scale/scale_mask.png);
+    
+    overflow: hidden;
+}
 
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
+#scale-base {
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    background-repeat:no-repeat;
+    background-image: url(img/assessment/scale/scale_base.png);
+}
+
+#scale-overlay {
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    background-repeat:no-repeat;
+    background-image: url(img/assessment/scale/scale_overlay.png);
+}
+
+</style>
 </head>
 <body>
-	<div class="welcome">
-		<h1>Nicks Route</h1>
-		<h1><?php echo App::environment();?> </h1>
-		<h1><?php echo getenv('TEST')?> </h1>
+
+
+
+	<div class="scale-container">
+		<div id="scale-base"></div>
+		<div id="scale-overlay"></div>
 	</div>
+
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+	<!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
+	<script
+		src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
+	<script
+		src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>
+	<script
+		src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
+
+
+	<script src="js/jam.js"></script>
+
 </body>
 </html>
--- public/js/jam.js
+++ public/js/jam.js
@@ -3,38 +3,49 @@ $(document).ready(
 		function() {
 
 			// Touch events
-			var touchStartPos;
-			var allCards = $('div[class^="card-"]');
+			var touchStartPos,
 			moved = 0, // amount your finger moved during touchmove
-			isTouchDevice = false; // set to true on touchstart
+			isTouchDevice = false, // set to true on touchstart
+			scaleBase = $('#scale-base'),
+			rot = 0;
 
-			$(window).bind(
-					'touchstart',
-					function(e) {
-						e.preventDefault();
-						// isTouchDevice = true;
-						clearInterval(touchInterval);
-						moved = 0;
-						var touch = e.originalEvent.touches[0]
-								|| e.originalEvent.changedTouches[0];
-						touchStartPos = touch.pageY;
-					})
-			$('body').bind(
-					'touchmove',
-					function(e) {
-						e.preventDefault();
-						var touch = e.originalEvent.touches[0]
-								|| e.originalEvent.changedTouches[0];
-						var currentY = touch.pageY;
-						moved = currentY - touchStartPos;
-						touchStartPos = currentY;
-					});
-
-			$(window).bind(
-					'touchend',
-					function(e) {
-						e.preventDefault();
-						var touch = e.originalEvent.touches[0]
-								|| e.originalEvent.changedTouches[0];
-					})
+			$('.scale-container').bind('touchstart', function(e) {
+				e.preventDefault();
+				// isTouchDevice = true;
+				moved = 0;
+				var touch = e.originalEvent.touches[0]
+						|| e.originalEvent.changedTouches[0];
+				touchStartPos = touch.pageX;
+				startTrackingTouch();
+			})
+			
+			function startTrackingTouch() {
+				$('body').bind('touchmove', function(e) {
+					e.preventDefault();
+					var touch = e.originalEvent.touches[0]
+							|| e.originalEvent.changedTouches[0];
+					var currentX = touch.pageX;
+					moved = currentX - touchStartPos;
+					rot += moved/2;
+					rotate(scaleBase, rot);
+					touchStartPos = currentX;
+				});
+				$(window).bind('touchend', function(e) {
+					e.preventDefault();
+					var touch = e.originalEvent.touches[0]
+							|| e.originalEvent.changedTouches[0];
+//					console.log(moved);
+					endTrackingTouch();
+				})
+			}
+			
+			function endTrackingTouch() {
+				$( "body" ).unbind();
+				$( window ).unbind();
+			}
+			
+			function rotate(obj, degree) {
+				TweenLite.to('#scale-base', 1, {css:{rotation:degree}, ease:Power3.easeOut});
+	        }
+			
 		});

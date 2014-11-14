daeca909f0ed1073855c6b5b3e4652784e21521e
Nick Velloff <nick.velloff@gmail.com>
2014-11-14 11:17:10 -0500

    chart builder work

---
+++ app/views/charts.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>chart builder</title>
+<link rel="stylesheet" href="css/chart-builder.css">
+</head>
+<body>
+	
+	<div id="donut-chart"></div>
+	
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+	<!--CDN links for the latest TweenLite, CSSPlugin, and EasePack-->
+	<script
+		src="//cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
+	<script
+		src="//cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>
+	<script
+		src="//cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
+		
+	<script src="//d3js.org/d3.v3.min.js"></script>
+	<script src="js/chartbuilder.js"></script>
+
+</body>
+	<script type="text/javascript">
+		
+		$( document ).ready(function() {
+			//var im = new IntroModule();
+			//im.init('#numbers-svg-container');
+		});
+	</script>
+</html>

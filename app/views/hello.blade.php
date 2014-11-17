43676983b69be9b6de9ecf2374378ee65b566b95
Nick <nick@narrowdesign.com>
2014-11-17 01:59:30 -0500

    removed slider

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -381,12 +381,14 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
+        <script src="js/main.js"></script>
+
+<!--    
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/easing/EasePack.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js"></script>
-        <script src="js/main.js"></script>
-        <script src="js/weight.js"></script>
-		<script src="js/height.js"></script>
+		<script src="js/weight.js"></script>
+		<script src="js/height.js"></script>-->
 
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];

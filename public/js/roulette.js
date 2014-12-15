fcbaced3b14f288f5a66c617e00bfce186ed4c91
Nick Velloff <nick.velloff@gmail.com>
2014-12-15 01:32:49 -0500

    roulette complete

---
+++ public/js/roulette.js
@@ -0,0 +1,132 @@
+var wheel = $(document).ready(
+		function() {
+			/*
+			 * SET doAllowDrag to false to disable the mouse spin
+			 */
+			var doAllowDrag = true;
+			var touchStartPos,
+			moved = 0, // amount your finger moved during touchmove
+			wheelBase = $('#wheel-base'),
+			rot = 0;
+			var touch;
+			
+			var main_chart_div = d3.select("#wheel-overlay").append("div");
+			var svg;
+			d3.xml("img/roulette/roulette_spencer.svg", function(error,
+					documentFragment) {
+				if (error) {
+					console.log(error);
+					return;
+				}
+				var svgNode = documentFragment.getElementsByTagName("svg")[0];
+				main_chart_div.node().appendChild(svgNode);
+				svg = main_chart_div.select("svg")
+				doSetHighlighted('you')
+			})
+			
+			var locations = [348, 8, 54, 94, 142, 166, 220, 288, 347];
+			var ids = ['you', 'girl', 'work', 'sister', 'mom', 'maid', 'friend', 'partner'];
+			
+			function highlightByRotation() {
+				var r1 = wheelBase[0]._gsTransform.rotation % 360 + 12;
+				var pos = 0;
+				for (var i=0; i<locations.length; i++) {
+					var l1 = (locations[i] + 12) % 360;
+					var l2 = (locations[(i+1) % locations.length] + 12) % 360;
+					if (r1 >= l1 && r1 <= l2) {
+						pos = i;
+						break;
+					}
+				}
+				doSetHighlighted(ids[i]);
+			}
+			
+			function doSetHighlighted(segmentId) {
+				svg.selectAll('text').style('fill', "#D8076D")
+				svg.selectAll('path').style('fill', "none")
+				
+				svg.selectAll('#' + segmentId + ' text').style('fill', "#fff")
+				svg.selectAll('#path' + segmentId + ' path').style('fill', "#D8076D")
+			}
+			
+			if (doAllowDrag) {
+				wheelBase.mousedown (function(e) {
+					e.preventDefault();
+					moved = 0;
+					touch = e;
+					touchStartPos = touch.pageX;
+					startTrackingTouch(false);
+				});
+				
+				wheelBase.bind('touchstart', function(e) {
+					e.preventDefault();
+					moved = 0;
+					touch = e.originalEvent.touches[0]
+						|| e.originalEvent.changedTouches[0];
+					touchStartPos = touch.pageX;
+					startTrackingTouch(true);
+				});
+			}
+			
+			
+			function startTrackingTouch(isTouch) {
+								
+				var currentX;
+				
+				if (isTouch) {
+					$(window).bind('touchmove', function(e) {
+						e.preventDefault();
+						touch = e.originalEvent.touches[0]
+						|| e.originalEvent.changedTouches[0];
+						move(touch);
+					});
+					
+					$(window).bind('touchend', function(e) {
+						e.preventDefault();
+						endTrackingTouch();
+					});
+					
+				} else {
+					
+					$(window).mousemove(function(e) {
+						e.preventDefault();
+						touch = e;
+						move(touch);
+					});
+					
+					$(window).mouseup(function(e) {
+						e.preventDefault();
+						endTrackingTouch();
+					});
+				}
+			}
+			
+			
+			function move(touch) {
+				currentX = touch.pageX;
+				moved = currentX - touchStartPos;
+				rot += moved * 10;
+				rotate(wheelBase, rot, 10);
+//				rot += moved*2;
+//				rotate(wheelBase, rot, 1);
+				touchStartPos = currentX;
+			}
+				
+			function endTrackingTouch() {
+								
+				$( window ).unbind('touchmove');
+				$( window ).unbind('touchend');
+				$( window ).unbind('mousemove');
+				$( window ).unbind('mouseup');
+			}
+			
+			function rotate(obj, degree, time) {
+				TweenLite.killTweensOf(obj);
+				TweenLite.to(obj, time, {css:{rotation:degree}, ease:Power3.easeOut, onUpdate:applyValue, onUpdateParams:[degree]});
+	        }
+
+			function applyValue(degree) {
+				highlightByRotation()
+			}
+			
+		});

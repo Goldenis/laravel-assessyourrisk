c86310594c8a382663653d3ff1790f4ed1a7029d
narrowdesign <nick@narrowdesign.com>
2015-03-07 10:29:11 -0500

    mobile wheel

--- public/js/anim.js
+++ public/js/anim.js
@@ -13,12 +13,12 @@
 
     //instantiate a TimelineLite    
     var tl = new TimelineLite();
-
-    tl.from(wheel, 1, {opacity:0, scale:.2}, "+=.1");
     if($(window).width() < 768){
         minS = .7;
+        tl.from(wheel, 1, {opacity:0, scale:.2}, "+=.1");
     }else{
         minS = .8;
+        tl.from(wheel, 1, {opacity:0, y: 10, scale:.2}, "+=.1");
     }
     var wheelScale = Math.max(minS,1 * ($(window).width()/1024));
     // if($(window).width() <= 1024){
@@ -27,9 +27,9 @@
     // if($(window).width() <= 768){
     //   wheelScale = Math.min(1,($(window).width()/320) * .6);
     // }
-    tl.to(wheel, 2, {scale:wheelScale, ease:Expo.easeOut})
-    tl.to(logo, .5, {opacity:1}, "+=0");
-    tl.from(second, 0.5, {opacity:0}, "-=.2");
+    tl.to(wheel, 2, {scale:wheelScale, y: 10, ease:Expo.easeOut})
+    tl.to(logo, 1.5, {opacity:1}, "+=0");
+    tl.from(second, 1.5, {opacity:0}, "+=2.1");
 
     //add a label 0.5 seconds later to mark the placement of the next tween
     tl.add("stagger", "+=0.2")

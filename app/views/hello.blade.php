0e44b188c5b8f6a947300e1faddd50c0b8597cb1
Spencer Harrison <spencerjharrison@gmail.com>
2015-03-12 11:34:07 -0400

    new logo files and readmore results

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -52,7 +52,7 @@
   <body>
     <div class="border"></div>
     <div class="logo">
-      <a href="http://www.brightpink.org/" target="_blank"><img src="img/brightpink_logo.png"></a>
+      <a href="http://www.brightpink.org/" target="_blank"><img src="img/AssessYourRiskLogo_horizontal_RGB_BPlockup.png"></a>
 
     </div>
     <div class="email-content"></div>
@@ -132,15 +132,17 @@ Bright Pink—a non-profit organization focused on saving women’s lives from b
                 This means you have a 12% chance of getting breast cancer—that’s one in eight women—and a 1.5% chance of getting ovarian cancer.  
                 75% of all breast and ovarian cancers are diagnosed in average risk women, so being proactive about <a href="http://www.brightpink.org/what-you-need-to-know/reduce-your-risk/" target="_blank">risk-reduction</a> and <a href="http://www.brightpink.org/what-you-need-to-know/early-detection/" target="_blank">early detection</a> is still important.
                 </p>
-                <div class="triggered-cancer-copy average more-results">
-                  <h5 class="column-header pink-text">Since You've Been Diagnosed With Breast or Ovarian Cancer:</h5>
-                    <p>
-                      It may seem like being at “average risk” when you’ve already been diagnosed with breast or ovarian cancer seems strange, but as noted above, the majority of breast and ovarian cancers are diagnosed in women with average risk.  
-                      The information below may be less relevant to you now, post-diagnosis, but we still recommend bringing it to your doctor to discuss which strategies you should still incorporate (most of these recommendations are good to keep in mind for general health anyway).  
-                      <b>And the most important thing we can recommend is talking to your doctor or a genetic counselor about pursing genetic testing, if you haven’t already had it.</b>  
-                      This testing will help determine if your cancer was likely the result of a gene mutation.  
-                      If it was, your baseline risk is actually higher than average and you will need to discuss enhanced risk management strategies with your doctor.
-                    </p>
+                <div class="more-results">
+                  <div class="triggered-cancer-copy average">
+                    <h5 class="column-header pink-text">Since You've Been Diagnosed With Breast or Ovarian Cancer:</h5>
+                      <p>
+                        It may seem like being at “average risk” when you’ve already been diagnosed with breast or ovarian cancer seems strange, but as noted above, the majority of breast and ovarian cancers are diagnosed in women with average risk.  
+                        The information below may be less relevant to you now, post-diagnosis, but we still recommend bringing it to your doctor to discuss which strategies you should still incorporate (most of these recommendations are good to keep in mind for general health anyway).  
+                        <b>And the most important thing we can recommend is talking to your doctor or a genetic counselor about pursing genetic testing, if you haven’t already had it.</b>  
+                        This testing will help determine if your cancer was likely the result of a gene mutation.  
+                        If it was, your baseline risk is actually higher than average and you will need to discuss enhanced risk management strategies with your doctor.
+                      </p>
+                  </div>
                 </div>
               </div>
               <!-- paragraph-two (right) -->
@@ -236,12 +238,14 @@ Bright Pink—a non-profit organization focused on saving women’s lives from b
                   This is significant, so it’s a great thing that you’ve identified this risk and are here learning more about the <a href="http://www.brightpink.org/what-you-need-to-know/reduce-your-risk/#high-risk-reduction" target="_blank">risk-reduction</a> and <a href="http://www.brightpink.org/what-you-need-to-know/early-detection/#high-risk-screening-recommendation" target="_blank">early detection</a> options that are available to you.  
                   Living a proactive lifestyle is one of the most important things you can do!
                 </p>
-                <div class="triggered-cancer-copy high">
-                  <h5 class="column-header pink-text">Since You've Been Diagnosed With Breast or Ovarian Cancer:</h5>
-                    <p>
-                      Some of the risk-reduction and early detection information below may be less relevant to you now, post-diagnosis.  
-                      We still recommend bringing these results to your doctor to discuss which strategies you may still need to incorporate.
-                    </p>
+                <div class="more-results">
+                  <div class="triggered-cancer-copy high">
+                    <h5 class="column-header pink-text">Since You've Been Diagnosed With Breast or Ovarian Cancer:</h5>
+                      <p>
+                        Some of the risk-reduction and early detection information below may be less relevant to you now, post-diagnosis.  
+                        We still recommend bringing these results to your doctor to discuss which strategies you may still need to incorporate.
+                      </p>
+                  </div>
                 </div>
               </div>
               <!-- paragraph-two (right) -->
@@ -530,7 +534,7 @@ Bright Pink—a non-profit organization focused on saving women’s lives from b
         <div class="assess">
           <h4>Assess<br>Your Risk<br></h4>
           <div class="arrow"><img src="img/arrow_left.png"></div>
-          <div class="logo-white"><img src="img/brightpink_logo_white.png"></div>
+          <div class="logo-white"><img src="img/AssessYourRiskLogo_horizontal_RGB_BPlockup.png"></div>
         </div>
       </div>
     </div>

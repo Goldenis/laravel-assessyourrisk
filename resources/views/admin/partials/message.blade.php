c38cde9d7e42f582d2783fa27ca8ab89ec6bbee8
Eduardo Valenzuela <evalenzuela@catarxis.com>
2015-10-01 19:50:46 -0500

    no message

---
+++ resources/views/admin/partials/message.blade.php
@@ -0,0 +1,6 @@
+@if(\Session::has('message'))
+    <div class="alert alert-success alert-dismissible fade in message" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+        {{\Session::get('message')}}
+    </div>
+@endif
